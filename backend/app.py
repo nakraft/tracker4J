99ea0e6cedb7f31b84375042e44ebdb65675f976
aramasw <aramasw@ncsu.edu>
2022-12-05 18:52:35 -0500

    Added changes from app.py

--- backend/app.py
+++ backend/app.py
@@ -16,15 +16,6 @@ UserRecords = db.register
 Applications = db.Applications
 UserProfiles = db.Profiles
 
-@app.route("/test", methods=["get"])
-def test() :
-    today = str(datetime.today()).split()[0]
-    query = {"date": {"$gte": today}}
-    projection = {"date":1, "_id":0, "companyName": 1, "jobTitle": 1}
-    item_details = Applications.find(query,projection)
-    print(item_details[0])
-    return jsonify(list(item_details))
-
 @app.route("/register", methods=["post"])
 def register():
     try:
@@ -371,6 +362,25 @@ def clear_profile():
         print(e)
         return jsonify({'error': "Something went wrong"}), 400
 
+@app.route("/interviews", methods=["get"])
+def test() :
+    try:
+        if request:
+            req = request.get_json()
+            email = req.args.get["email"]
+            today = str(datetime.today()).split()[0]
+            query = {"email": email, "Interview_date": {"$gte": today}}
+            projection = {"date":1, "_id":0, "companyName": 1, "jobTitle": 1}
+            item_details = Applications.find(query,projection)
+            print(item_details)
+            if item_details == None:
+                return jsonify({"error": "No such Job ID found for this user's email"}), 400
+            else: 
+                return jsonify(list(item_details))
+            print(item_details[0])
+    except Exception as e:
+        print(e)
+        return jsonify({'error': "Something went wrong"}), 400
 
 if __name__ == "__main__":
   app.run(debug=True, host="0.0.0.0", port=8000)
